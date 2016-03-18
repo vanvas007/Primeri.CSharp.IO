496a8a1a12250b7852d9697aafa828db2f17fb9a
i.vasilev <vanvas62@gmail.com>
2016-03-18 10:50:23 +0200

    Модул 08, Видео 03 - Четене на текстов файл

--- IOTesting/IOTextFiles/IOSettings.cs
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -11,6 +11,12 @@ namespace IOTextFiles
 			_stable = stable;	
 		}
 
+		public string getPath ()
+		{
+			string _path = System.IO.Path.Combine ( AppDomain.CurrentDomain.BaseDirectory, "test.txt");
+			return _path;
+		}
+
 		public bool save ()
 		{
 			try
@@ -20,13 +26,34 @@ namespace IOTextFiles
 				_temp = String.Join ( "\r\n" , _stable.stable);
 
 				//Запис на текстов файл
-				System.IO.File.WriteAllText("C:\\Aula\\test.txt", _temp);
+				System.IO.File.WriteAllText(getPath (), _temp);
+
 				return true;
 			}catch{
 			}
 
 			return false;
 		}
+
+		public bool open ()
+		{
+			try
+			{
+
+				string _temp = System.IO.File.ReadAllText ( getPath() );
+
+				string[] _table = _temp.Replace("\r","").Split ('\n');
+
+				for ( int i = 0; i< _table.Length; i++)
+				{
+					_stable.stable[i]= _table[i];
+				}
+				return true;
+			}catch{
+			}
+			return false;
+		}
+
 	}
 }
 
--- IOTesting/IOTextFiles/Program.cs
+++ IOTesting/IOTextFiles/Program.cs
@@ -11,13 +11,14 @@ namespace IOTextFiles
 			STable _Stable = new STable ();
 			IOSettings _io = new IOSettings (_Stable);
 
-			Console.WriteLine ( _Stable.stable [0] );
+			//Console.WriteLine ( _Stable.stable [0] );
 
-			if (_io.save ()) {
-				Console.WriteLine ("Таблицата е запаметена успешно!");
+			if (_io.open () ) {  //_io.save ()) {
+				Console.WriteLine ("Таблицата е прочетене успешно!");
 			} else {
-				Console.WriteLine ("Таблицата НЕ запаметена успешно!");	
+				Console.WriteLine ("Таблицата НЕ Е прочетена успешно!");	
 			}
+			Console.WriteLine ( _Stable.stable [2] );
 
 		}
 	}
