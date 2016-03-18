6d504c58970ed09bbb72eb363449e3d898a3e54a
i.vasilev <vanvas62@gmail.com>
2016-03-18 12:01:27 +0200

    Модул 08, Видео 06 - Създаване на директория

--- IOTesting/IOTextFiles/IOSettings.cs
+++ IOTesting/IOTextFiles/IOSettings.cs
@@ -13,11 +13,15 @@ namespace IOTextFiles
 
 		public string getPath ()
 		{
-			string _path = System.IO.Path.Combine ( AppDomain.CurrentDomain.BaseDirectory, "test.txt");
-
+			//Програма\Settings\settings.txt
+			string _path = System.IO.Path.Combine ( AppDomain.CurrentDomain.BaseDirectory, "Settings");
+			_path = System.IO.Path.Combine ( _path, "settings.txt");
 			//Други видове директории
-			string _user = Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData); Console.WriteLine ( _user);
-			string _desktop = Environment.GetFolderPath (Environment.SpecialFolder.Desktop);            Console.WriteLine ( _desktop);
+			//string _user = Environment.GetFolderPath (Environment.SpecialFolder.LocalApplicationData); 
+			//Console.WriteLine ( _user);
+
+			//string _desktop = Environment.GetFolderPath (Environment.SpecialFolder.Desktop);
+			//Console.WriteLine ( _desktop);
 				
 
 			return _path;
@@ -45,8 +49,11 @@ namespace IOTextFiles
 		{
 			try
 			{
+				iniSettings ();
 
 				string _temp = "", _filePath = getPath();
+
+
 
 				if ( System.IO.File.Exists(_filePath)) //проверка за валидност на пътя
 				{
@@ -60,7 +67,7 @@ namespace IOTextFiles
 					_stable.stable[i]= _table[i];
 				}
 				}else {
-					Console.WriteLine("Не е намерен такъв път!");					
+					Console.WriteLine("Не е намерен такъв път!"); 					
 					return false;
 				}
 
@@ -70,6 +77,32 @@ namespace IOTextFiles
 			return false;
 		}
 
+
+		private void iniSettings()
+		{
+			try
+			{
+				bool _fileExist = System.IO.File.Exists( getPath ());
+
+				if (!_fileExist)
+				{
+					string _directory = System.IO.Path.GetDirectoryName ( getPath () );
+
+					//Застраховаме се, че директорията съществува!
+					if (!System.IO.Directory.Exists (_directory ))
+					{
+						System.IO.Directory.CreateDirectory ( _directory);
+					}
+
+
+					//Сапаметяване съдържанието на файла.
+					save ();
+				}
+
+
+			}catch{
+			}
+		}
 	}
 }
 
